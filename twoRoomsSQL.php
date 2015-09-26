f7019be0e241eddd934f316a213780ab58ec0ef3
findusl <sebastian@lehrbaum.de>
2015-09-27 01:30:36 +0200

    Implemented Database sync.
    Database download now mostly working. Only missing replacement of escape
    characters.
    Replaced almost all 4 spaces with tabs in the files.

--- twoRoomsSQL.php
+++ twoRoomsSQL.php
@@ -17,7 +17,7 @@ if($db->connect_errno > 0){
 
 $since = $_GET['since'];
 
-function echoTable($db, $since, $table, $columns) {
+function echoTable($db, $since, $table, $columns, $intColumns) {
 	//echo 'reading table ' . $table . '<br>';
 	//echo date("Y-m-d H-i-s" , $since) . '<br>';
 	$result = mysqli_query($db, "SELECT " . $columns . " FROM " . $table . " WHERE last_modified >= '" . date("Y-m-d H-i-s" , $since) . "'");
@@ -26,6 +26,9 @@ function echoTable($db, $since, $table, $columns) {
 	$tableAsArray = array();
 
 	while($row = mysqli_fetch_row($result)){
+		foreach($intColumns as $index) {
+			$row[$index] = intval($row[$index]);
+		}
 		//echo 'Getting row ' . json_encode($row) . '<br>';
 		$tableAsArray[] = $row;
 	}
@@ -40,15 +43,15 @@ function echoTable($db, $since, $table, $columns) {
 
 $resultAsArray = array();
 
-$resultAsArray['categories'] = echoTable($db, $since, 'categories', '_id, name');
-$resultAsArray['roles'] = echoTable($db, $since, 'roles', '_id, name, description, team_id, groupe, category');
-$resultAsArray['sets'] = echoTable($db, $since, 'sets', '_id, name, count, parent, description');
-$resultAsArray['set_roles'] = echoTable($db, $since, 'set_roles', 'id_set, id_role');
-$resultAsArray['teams'] = echoTable($db, $since, 'teams', '_id, name');
+$resultAsArray['categories'] = echoTable($db, $since, 'categories', '_id, name', array(0));
+$resultAsArray['roles'] = echoTable($db, $since, 'roles', '_id, name, description, team_id, groupe, category', array(0, 3, 4, 5));
+$resultAsArray['sets'] = echoTable($db, $since, 'sets', '_id, name, count, parent, description', array(0, 2, 3));
+$resultAsArray['set_roles'] = echoTable($db, $since, 'set_roles', 'id_set, id_role', array(0, 1));
+$resultAsArray['teams'] = echoTable($db, $since, 'teams', '_id, name', array(0));
 
 //echo 'finished reading tables' . '<br>';
 
-$json_string = json_encode($resultAsArray);
+$json_string = 'time:' . time() . json_encode($resultAsArray);
 $length = strlen($json_string);
 echo $length . $json_string;
 //echo the length at the start so the android knows how big the array has to be.
