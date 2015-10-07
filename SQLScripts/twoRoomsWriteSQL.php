5f64f98a6d4b977636d4ca513be1f7f0c8fa9482
Sebastian Lehrbaum <sebastian@lehrbaum.de>
2015-10-07 14:06:49 +0200

    Minor: working on Sync

--- SQLScripts/twoRoomsWriteSQL.php
+++ SQLScripts/twoRoomsWriteSQL.php
@@ -6,15 +6,15 @@ $sets = json_decode($_POST["sets"], true);
 $maps = array();
 foreach ($sets as $set) {
 	//check parent
-	$parent = $set["parent"];
+	$parent = $set[3];
 	if($parent != -1) {
 		$mapping = $maps[$parent];
 		if($mapping != null)
 			$parent = $mapping;
 	}
-    $id = "INSERT INTO sets (name, description, count, parent) VALUES(".$set["name"].",".$set["description"].",".$set["count"].",".$parent.")";
-	$maps[$set["id"]] = $id;
-	foreach ($set["roles"] as $role) {
+    $id = "INSERT INTO sets (name, count, parent, description) VALUES(".$set[1].",".$set[2].",".$set[3].",".$parent.")";
+	$maps[$set[0]] = $id;
+	foreach ($set[5] as $role) {
 		"INSERT INTO set_role VALUES (".$id.",".$role")";
 	}
 }
